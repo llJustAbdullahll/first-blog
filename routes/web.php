ec7c5cb98b768d891a08afcde3f5e8fddca63277
justabdullah <llljustabdullahlll@gmail.com>
2026-06-20 08:57:32 +0300

    Last Edit The last modification was adding a package to add authentication.

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
-Route::get('/', function () {
-    // return view('welcome');
-
-    return 'Abdullah';
-});
-
 Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
 Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
 Route::post('posts', [PostController::class, 'store'])->name('posts.store');
