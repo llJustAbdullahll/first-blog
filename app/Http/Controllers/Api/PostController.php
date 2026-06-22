f76af2aa216c037122a71cfe5d9831b4baca5b35
justabdullah <llljustabdullahlll@gmail.com>
2026-06-22 15:51:21 +0300

    Add Api/PostController And PostResource

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Http\Resources\PostResource;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $post = Post::all();
+        return PostResource::collection($post);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
-use App\Http\Controllers\Api\ApiPostController;
 
 Route::get('/', function () {
     // return view('welcome');
@@ -11,7 +10,7 @@ Route::get('/', function () {
 });
 
 Route::get('/posts', [PostController::class, 'index'])->name('posts.index')->middleware('auth');
-Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create'); 
+Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
 Route::post('posts', [PostController::class, 'store'])->name('posts.store');
 Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
 Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
@@ -21,5 +20,3 @@ Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-
