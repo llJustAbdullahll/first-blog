4af2749e46d99e6960cf2d3a5e76de5e3f83c113
justabdullah <llljustabdullahlll@gmail.com>
2026-06-22 21:27:13 +0300

    Add UserResource

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class PostResource extends JsonResource
+class UserResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -16,9 +16,8 @@ class PostResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'title' => $this->title,
-            'description' => $this->description,
-            'user' => new UserResource($this->user)
+            'name' => $this->name,
+            'email' => $this->email
         ];
     }
 }
